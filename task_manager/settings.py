8b4f68befe87b9fb603a589f95bb0159dbdb25b8
Viacheslav Kurganov <slavanik2506@gmail.com>
2025-06-06 13:18:12 +0300

    Added staticfiles directory, updated .gitignore and settings.py

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -146,6 +146,7 @@ LOCALE_PATHS = [BASE_DIR / 'locale']
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
